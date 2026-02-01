0c581befc9abff581094e87f5d337809e8960d44
Kavali Hemalatha <hemalathakavali1998@gmail.com>
2026-02-01 14:13:12 +0530

    shell script

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+# while declaring variables, no space shound be there between value and equal.
+
+
+echo "$person1 :: Hi $person2, how are you?"
+echo "$person2 :: Hello $person1, Im good. How are you doing?"
+echo "$person1 :: Im doing good. What are yoy learning?"
+echo "$person2 :: Im learning shell script. What about you?"
+echo "$person1 :: Nice! Im learning Git"
