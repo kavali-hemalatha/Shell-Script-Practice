f9424185d111adbcbc8fb88f9e4ff1156c4e4e16
Kavali Hemalatha <hemalathakavali1998@gmail.com>
2026-02-01 13:45:39 +0530

    shell script

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hema :: Hi Latha, how are you?"
+echo "Latha :: Hello Hema, Im good. How are you doing?"
+echo "Hema :: Im doing good. What are yoy learning?"
+echo "Latha :: Im learning shell script. What about you?"
+echo "Hema :: Nice! Im learning Git"
