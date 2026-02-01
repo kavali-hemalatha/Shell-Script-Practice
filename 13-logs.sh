e9b32f87c28af9d09d29fd098be89e99e8c9e0c7
Kavali Hemalatha <hemalathakavali1998@gmail.com>
2026-02-01 23:08:48 +0530

    shell script

---
+++ 13-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+user_id=$(id -u)
+log_folder="/var/log/shell-script"
+log_file="/var/log/shell-script/$0.log"
+
+
+if [ $user_id -ne 0 ]; then
+echo "this script needs to be run with root user"
+exit 1
+fi
+
+mkdir -p $log_folder
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+echo "$2.....FAILURE"
+exit 1
+else
+echo "$2.....SUCCESS"
+fi
+}
+echo "installing Nginx..."
+dnf install nginx -y &>> $log_file
+VALIDATE $? "installing nginx"
+
+dnf install mysql -y &>> $log_file
+VALIDATE $? "installing mysql"
+
+dnf install nodejs -y &>> $log_file
+VALIDATE $? "installing nodejs"
