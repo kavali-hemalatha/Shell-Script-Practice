0ceaa2dd12069daff46b92dd2aa3690929f28620
Kavali Hemalatha <hemalathakavali1998@gmail.com>
2026-02-02 13:37:00 +0530

    shell script

---
+++ 18-colours_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[39m'
+
+user_id=$(id -u)
+log_folder="/var/log/shell-script"
+log_file="/var/log/shell-script/$0.log"
+
+if [ $user_id -ne 0 ]; then
+echo -e "$R this script needs to be run with root user" | tee -a $log_file
+exit 1
+fi
+
+mkdir -p $log_folder
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+echo -e "$R $2.....FAILURE" | tee -a $log_file
+exit 1
+else
+echo -e "$G $2.....SUCCESS" | tee -a $log_file
+fi
+}
+
+for package in $@
+do
+  dnf list installed $package &>> $log_file
+  if [ $? -ne 0 ]; then
+     echo "$package not installed, installing now"
+     dnf install $package -y &>> $log_file
+     VALIDATE $? "$package installing"
+  else
+     echo -e "$Y $package already installed, skipping"
+  fi
+done
