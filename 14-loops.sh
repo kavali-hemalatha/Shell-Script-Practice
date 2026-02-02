23a67d0d6a137c73eb0f1e9dd3d5c947e5e40128
Kavali Hemalatha <hemalathakavali1998@gmail.com>
2026-02-02 12:28:04 +0530

    shell script

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+###2 dots refers to expansion where as more than 2 dots refers to just a string. it treats as a string.
+
+##If you write three dots ({1...100}), Bash does not recognize it as valid syntax.
+##It treats it as a literal string instead of a range, so the loop variable i just becomes the string "{1...100}".
+
 for i in {1..100}
 do
    echo $i
